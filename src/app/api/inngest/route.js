29c93671a7f473c5bacce83d307f48b341c9eb58
sekoia929-ui <sekoia929@gmail.com>
2026-08-04 03:31:08 +0500

    Add maxDuration constant to route.js

--- src/app/api/inngest/route.js
+++ src/app/api/inngest/route.js
@@ -2,6 +2,8 @@ import { serve } from 'inngest/next'
 import { inngest } from '@/lib/inngest'
 import { extractPaper } from '@/inngest/functions'
 
+export const maxDuration = 60
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [extractPaper]
